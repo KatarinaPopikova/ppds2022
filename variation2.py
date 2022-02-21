44d6f3e2819b31e0c9ce96cdc58e8e838e5e6405
kStasova <stasovakatka6688@gmail.com>
2022-02-21 18:47:19 +0100

    chore: initialization variation2.py by copying code from variation1.py

---
+++ variation2.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+from collections import Counter
+from fei.ppds import Thread
+
+
+class Shared:
+    """Shared array for threads"""
+
+    def __init__(self, size):
+        """Create shared zero array with the size for increasing.
+        Initialize counter to 0. This counter will point to index of array.
+
+        :param size: size of shared array
+        """
+        self.counter = 0
+        self.end = size
+        self.elms = [0] * size
+
+
+def do_count(shared):
+    """ Function for increasing the values of array.
+
+    :param shared: instance of Shared class
+    """
+    while shared.counter < shared.end:
+        shared.elms[shared.counter] += 1
+        shared.counter += 1
+
+
+size = 1_000_000
+shared = Shared(size)
+
+t1 = Thread(do_count, shared)
+t2 = Thread(do_count, shared)
+
+t1.join()
+t2.join()
+
+counter = Counter(shared.elms)
+print(counter.most_common())
+
+plt.hist(shared.elms, bins=len(counter), align='mid')
+plt.xticks(range(len(counter)))
+plt.show()
