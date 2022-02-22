03523b6a280c180500fcacbeacfc27365af86210
kStasova <stasovakatka6688@gmail.com>
2022-02-22 07:27:51 +0100

    docs: add param mutex description

--- variation_2.py
+++ variation_2.py
@@ -21,6 +21,7 @@ def do_count(shared, mutex):
     """ Function for increasing the values of array.
 
     :param shared: instance of Shared class
+    :param mutex: synchronization tool to make the critical area atomically executed
     """
     while True:
         mutex.lock()
