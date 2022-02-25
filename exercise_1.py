0160049d1533b8fad10ca055b527a6b8ed9a5ff1
kStasova <stasovakatka6688@gmail.com>
2022-02-25 22:15:09 +0100

    feat: create threads using newly added function

--- exercise_1.py
+++ exercise_1.py
@@ -1,4 +1,4 @@
-from fei.ppds import Semaphore, Mutex
+from fei.ppds import Thread, Semaphore, Mutex, print
 
 
 class SimpleBarrier:
@@ -18,6 +18,14 @@ class SimpleBarrier:
         self.T.wait()
 
 
+def use_barrier(barrier, thread_id):
+    print("Thread %d before barrier" % thread_id)
+    barrier.wait()
+    print("Thread %d after barrier" % thread_id)
+
+
 if __name__ == '__main__':
     thread_count = 5
     sb = SimpleBarrier(thread_count)
+    threads = [Thread(use_barrier, sb, i) for i in range(thread_count)]
+    [t.join() for t in threads]
