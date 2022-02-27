018ef1d7fdfd3cf58c0f6365aa354b7204dd6631
kStasova <stasovakatka6688@gmail.com>
2022-02-27 19:40:56 +0100

    feat: compute fibonacci without barrier

--- assignment_3.py
+++ assignment_3.py
@@ -3,14 +3,37 @@ from random import randint
 from fei.ppds import Thread, Semaphore, Mutex, Event
 
 
-def compute_fibonacci(fib_seq, i):
+class FibonacciIndex:
+    def __init__(self):
+        self.index = 0
+
+    def get_index(self):
+        return self.index
+
+    def set_index(self):
+        self.index += 1
+
+
+def compute_fibonacci_without_barrier(f_i, fib_seq, i):
     sleep(randint(1, 10) / 10)
+    mutex = Mutex()
+    while True:
+        mutex.lock()
+        sleep(randint(1, 10) / 10)
+        if f_i.get_index() == i:
+            f_i.set_index()
+            break
+        mutex.unlock()
+
     fib_seq[i + 2] = fib_seq[i] + fib_seq[i + 1]
+    mutex.unlock()
 
 
 if __name__ == '__main__':
     THREADS = 5
     fib_seq = [0] * (THREADS + 2)
-    threads = [Thread(compute_fibonacci, fib_seq, i) for i in range(THREADS)]
+    fib_seq[1] = 1
+    f_i = FibonacciIndex()
+    threads = [Thread(compute_fibonacci_without_barrier, f_i, fib_seq, i) for i in range(THREADS)]
     [t.join() for t in threads]
     print(fib_seq)
