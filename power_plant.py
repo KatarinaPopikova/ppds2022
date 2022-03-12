e1e757faf7a52d56dd04d16b40a3bdbcb5bb2838
kStasova <stasovakatka6688@gmail.com>
2022-03-12 22:49:37 +0100

    docs: add modul head

---
+++ power_plant.py
@@ -0,0 +1,7 @@
+""""Author: Katarína Stasová
+    License: MIT
+    Use of multiple synchronization objects in the program. The sensors update the data and the monitors display them
+    updated in the power plant."""
+
+
+
