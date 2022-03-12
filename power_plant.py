e219b1afc7dc3227a526ff1c6191cbbcae603a20
kStasova <stasovakatka6688@gmail.com>
2022-03-12 23:42:55 +0100

    chore: add methods

--- power_plant.py
+++ power_plant.py
@@ -3,5 +3,31 @@
     Use of multiple synchronization objects in the program. The sensors update the data and the monitors display them
     updated in the power plant."""
 
+from fei.ppds import Thread
 
 
+class Shared():
+    def __init__(self):
+        pass
+
+
+def monitor(thread_name):
+    print("%s" % thread_name)
+
+
+def sensor(thread_name):
+    print("%s" % thread_name)
+
+
+if __name__ == '__main__':
+    threads = list()
+    for i in {'P', 'T', 'H'}:
+        t = Thread(sensor, '%s' % i)
+        threads.append(t)
+
+    for i in range(8):
+        t = Thread(monitor, '%d' % i)
+        threads.append(t)
+
+    for t in threads:
+        t.join()
