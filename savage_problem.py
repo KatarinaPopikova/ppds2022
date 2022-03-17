c7fca5d591ef8afc0ec8b34a7adb82f9ecc460f2
kStasova <stasovakatka6688@gmail.com>
2022-03-17 23:32:49 +0100

    docs: add methods documentation

--- savage_problem.py
+++ savage_problem.py
@@ -1,6 +1,6 @@
 """"Author: Katarína Stasová
     License: MIT
-    Program of savage problem. Savages eat from pot and when pot is empty, savage wakes up the cooks. They cook and
+    Program of savage problem. Savages eat from pot and when pot is empty, savage wakes up the chefs. They cook and
     serve to pot. When pot is full, savages can continue to eat."""
 
 from random import randint
@@ -13,13 +13,29 @@ S = 5
 
 
 class EventBarrier:
+    """"A reusable barrier for waiting for all sensors to complete the part of code. It uses event. """
+
     def __init__(self, thread_count):
+        """Event barrier initialization:
+         all_thread_count - the number of sensors
+         count - the number of waiting threads in event
+         mutex - synchronization tool to make the critical area atomically executed
+         event - synchronization tool to management threads
+        :param thread_count: the number of sensors
+        """
         self.all_thread_count = thread_count
         self.count = 0
         self.mutex = Mutex()
         self.event = Event()
 
     def wait(self, m):
+        """Waiting until all sensors have completed part of the code.
+        Between locked mutex is code automatically executed.
+        The event method wait() blocks all threads, which invoke it, until method signal() happened.
+        The event method clear() activates wait(), when each thread is released from the barrier.
+        When count is full, that is, the chefs cooked m portions and filled the bowl. Savages can eat.
+        :param m: required number of portions
+        """
         self.mutex.lock()
         self.count += 1
         if self.count == self.all_thread_count:
@@ -36,7 +52,21 @@ class EventBarrier:
 
 
 class Shared:
+    """
+    Shared class for all threads.
+    """
+
     def __init__(self, m, s):
+        """
+        servings- the current number of portions in the pot
+        mutex - synchronization tool to make the critical area atomically executed
+        empty_pot- signals an empty pot
+        full_pot- signals an full pot
+        barrier- wait for all chefs
+
+        :param m: required number of servings
+        :param s: the number of chefs
+        """
         self.servings = m
         self.mutex = Mutex()
         self.empty_pot = Semaphore(0)
@@ -45,6 +75,13 @@ class Shared:
 
 
 def savage(i, shared, s):
+    """ Simulation of savages eating. Savage who find out, that the pot is empty wake up chefs. He is waiting untill the
+    pot is full.
+
+    :param i: savage id
+    :param shared: shared class for all threads
+    :param s: the number od savages
+    """
     sleep(randint(1, 100) / 100)
     while True:
 
@@ -61,12 +98,23 @@ def savage(i, shared, s):
 
 
 def eat(i):
+    """ Simulation of eating.
+
+    :param i: savage id
+    """
     print(f'savage {i}:eat start')
     sleep(randint(50, 200) / 100)
     print(f'savage {i}:eat end')
 
 
 def cook(cooker_id, shared, m):
+    """ Simulation of chefs cooking. When pot is empty, they wake up and start cooking together. The last chef serves
+    portions to the pot and savages can continue in eating.
+
+    :param cooker_id: cooker id
+    :param shared: shared class for all threads
+    :param m: required number of servings
+    """
     while True:
         shared.empty_pot.wait()
         print(f'cooker {cooker_id}: cooking')
@@ -76,6 +124,7 @@ def cook(cooker_id, shared, m):
 
 
 if __name__ == '__main__':
+    """Create threads."""
     shared = Shared(0, S)
     threads = list()
 
