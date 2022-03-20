1e7665b362f84c60d056e700846c9777f5945e68
kStasova <stasovakatka6688@gmail.com>
2022-03-20 18:02:00 +0100

    refactor: make more readable code

--- savage_problem.py
+++ savage_problem.py
@@ -28,18 +28,18 @@ class EventBarrier:
         self.mutex = Mutex()
         self.event = Event()
 
-    def wait(self, m):
+    def wait(self, portions_count):
         """Waiting until all sensors have completed part of the code.
         Between locked mutex is code automatically executed.
         The event method wait() blocks all threads, which invoke it, until method signal() happened.
         The event method clear() activates wait(), when each thread is released from the barrier.
         When count is full, that is, the chefs cooked m portions and filled the bowl. Savages can eat.
-        :param m: required number of portions
+        :param portions_count: required number of portions
         """
         self.mutex.lock()
         self.count += 1
         if self.count == self.all_thread_count:
-            shared.servings += m
+            shared.servings += portions_count
             shared.full_pot.signal()
             self.count = 0
             self.event.signal()
@@ -56,7 +56,7 @@ class Shared:
     Shared class for all threads.
     """
 
-    def __init__(self, m, s):
+    def __init__(self, portions_count, chefs_count):
         """
         servings- the current number of portions in the pot
         mutex - synchronization tool to make the critical area atomically executed
@@ -64,62 +64,64 @@ class Shared:
         full_pot- signals an full pot
         barrier- wait for all chefs
 
-        :param m: required number of servings
-        :param s: the number of chefs
+        :param portions_count: required number of servings
+        :param chefs_count: the number of chefs
         """
-        self.servings = m
+        self.servings = portions_count
         self.mutex = Mutex()
         self.empty_pot = Semaphore(0)
         self.full_pot = Semaphore(0)
-        self.barrier = EventBarrier(s)
+        self.barrier = EventBarrier(chefs_count)
 
 
-def savage(i, shared, s):
+def eat(savage_id):
+    """ Simulation of eating.
+
+    :param savage_id: savage id
+    """
+    print(f'savage {savage_id}:eat start')
+    sleep(randint(50, 200) / 100)
+    print(f'savage {savage_id}:eat end')
+
+
+def savage(savage_id, shared, chefs_count):
     """ Simulation of savages eating. Savage who find out, that the pot is empty wake up chefs. He is waiting until the
     pot is full.
 
-    :param i: savage id
+    :param savage_id: savage id
     :param shared: shared class for all threads
-    :param s: the number od savages
+    :param chefs_count: the number of chefs
     """
-    sleep(randint(1, 100) / 100)
     while True:
-
         shared.mutex.lock()
         if shared.servings == 0:
-            print(f'savage {i}: empty pot')
-            shared.empty_pot.signal(s)
+            print(f'savage {savage_id}: empty pot')
+            shared.empty_pot.signal(chefs_count)
             shared.full_pot.wait()
 
-        print(f'savage {i}: take from pot')
+        print(f'savage {savage_id}: take from pot')
         shared.servings -= 1
         shared.mutex.unlock()
-        eat(i)
+        eat(savage_id)
 
 
-def eat(i):
-    """ Simulation of eating.
-
-    :param i: savage id
-    """
-    print(f'savage {i}:eat start')
-    sleep(randint(50, 200) / 100)
-    print(f'savage {i}:eat end')
+def cooking(chef_id, portions_count):
+    print(f'chef {chef_id}: cooking')
+    sleep(randint(1, 4) / 10)
+    print(f'chef {chef_id}: cook {portions_count} servings --> pot')
 
 
-def cook(chef_id, shared, m):
+def cook(chef_id, shared, portions_count):
     """ Simulation of chefs cooking. When pot is empty, they wake up and start cooking together. The last chef serves
     portions to the pot and savages can continue in eating.
 
     :param chef_id: chef id
     :param shared: shared class for all threads
-    :param m: required number of servings
+    :param portions_count: required number of servings
     """
     while True:
         shared.empty_pot.wait()
-        print(f'chef {chef_id}: cooking')
-        sleep(randint(1, 4) / 10)
-        print(f'chef {chef_id}: cook {m} servings --> pot')
+        cooking(chef_id, portions_count)
         shared.barrier()
 
 
