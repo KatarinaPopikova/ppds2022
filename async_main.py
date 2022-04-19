521f4f1530b98002fed81186b44a36c3ead92e69
kStasova <stasovakatka6688@gmail.com>
2022-04-19 13:09:05 +0200

    docs: add function documentation

--- async_main.py
+++ async_main.py
@@ -8,6 +8,14 @@ import time
 
 
 async def get_capital_city(country):
+    """ The function is labeled `async`, so this is a native coroutine. It can be interrupted for lines marked with the
+    await command. It uses async frameworks for creating HTTP requests asynchronously, so it performs a context switch
+    back to the event loop. Code creates an aiohttp session context manager, then uses the session to get the text
+    retrieved from the URL asynchronously.
+    Coroutine select the capital city of country and find out the elapsed time.
+
+    :param country: country to find data
+    """
     url = 'https://restcountries.com/v3.1/name/' + country
     print(f"Get capital city of {country} from URL: {url}")
     time_start = time.perf_counter()
@@ -21,6 +29,10 @@ async def get_capital_city(country):
 
 
 async def main():
+    """ This coroutine call another coroutine to get the capital cities of the countries from the list.
+     The coroutine objects are created and run with the asyncio.gather () function. This feature schedules them and then
+     runs them competitively.
+    """
     start_time = time.perf_counter()
 
     await asyncio.gather(*[get_capital_city(country) for country in
@@ -32,6 +44,7 @@ async def main():
 
 
 if __name__ == "__main__":
+    """ This creates an event loop. This loop will run main()"""
     # Solve "RuntimeError: Event loop is closed" when ProactorEventLoop is used
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     asyncio.run(main())
