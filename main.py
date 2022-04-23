b605e9cc5732d10530c437f24f7bd88400e92acd
kStasova <stasovakatka6688@gmail.com>
2022-04-23 22:49:15 +0200

    feat: make an average of two images using cuda

---
+++ main.py
@@ -0,0 +1,40 @@
+import numpy
+from numba import cuda
+from math import ceil
+import cv2 as cv
+import time
+
+SIZE = 122
+
+
+@cuda.jit
+def my_kernel(data, data2):
+    x, y= cuda.grid(2)
+    x_max, y_max = data.shape[:2]
+    if x < x_max and y < y_max:
+        data[x][y][0] = (int(data[x][y][0]) + int(data2[x][y][0])) / 2
+        data[x][y][1] = (int(data[x][y][1]) + int(data2[x][y][1])) / 2
+        data[x][y][2] = (int(data[x][y][2]) + int(data2[x][y][2])) / 2
+
+
+def main():
+    original_img = cv.imread(cv.samples.findFile('resources/images/foto.jpg'))
+    original_img = cv.resize(original_img, (SIZE, SIZE), interpolation=cv.INTER_AREA)
+    img2 = cv.imread(cv.samples.findFile('resources/images/foto (1).jpg'))
+    img2 = cv.resize(img2, (SIZE, SIZE), interpolation=cv.INTER_AREA)
+    img = original_img.copy()
+    thread_per_block = (16, 16)
+    block_per_grid_x = ceil(img.shape[0] / thread_per_block[0])
+    block_per_grid_y = ceil(img.shape[1] / thread_per_block[1])
+    block_per_grid = (block_per_grid_x, block_per_grid_y)
+    start_time = time.perf_counter()
+    my_kernel[block_per_grid, thread_per_block](img, img2)
+    total_time = time.perf_counter() - start_time
+    print(f"\nTotal time: {total_time:.1f}")
+    cv.imshow('images', numpy.concatenate((original_img, img2, img), axis=0))
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
